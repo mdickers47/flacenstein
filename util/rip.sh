d9504381b1d57f9420c6c0b881b1ed22a7895275
Mikey Dickerson <pomonamikey@gmail.com>
2019-02-23 13:03:48 -0500

    fix a weird problem with some TOCs

--- util/rip.sh
+++ util/rip.sh
@@ -11,11 +11,18 @@ flac -o "$OUTFILE" --delete-input-file -V --padding 262144 cdda.wav
 
 echo 'Creating cuesheet'
 
-TRACKS=$(cdparanoia -Q 2>&1 | awk '/^ *[0-9]+\. / { print $5; }' | tr -d '[]' | tr '.' ':')
+# note that the extra ( ) causes bash to make TRACKS an array
+TRACKS=($(cdparanoia -Q 2>&1 | awk '/^ *[0-9]+\. / { print $5; }' | tr -d '[]' | tr '.' ':'))
+
+if [ "${TRACKS[0]}" != "00:00:00" ]; then
+  echo "warning: TOC wanted track 1 to start at ${TRACKS[0]} but it can't"
+  TRACKS[0]="00:00:00"
+fi
+
 I=1
 (
   echo 'FILE "cdda.wav" WAVE'
-  for T in $TRACKS ; do
+  for T in ${TRACKS[@]} ; do
     printf '  TRACK %02d AUDIO\n' $I
     printf '    INDEX 01 %s\n' $T
     I=$(($I + 1))
@@ -29,17 +36,20 @@ I=1
   echo 'ARTIST='
   echo 'ALBUM='
   echo 'DATE='
-  for T in $TRACKS ; do
+  for T in ${TRACKS[@]} ; do
     printf 'TITLE[%d]=\n' $I
     I=$(($I + 1))
   done
-) > tags
+) > tags.tmp
 
-$EDITOR tags
-metaflac --import-tags-from=tags "$OUTFILE"
+$EDITOR tags.tmp
+metaflac --import-tags-from=tags.tmp "$OUTFILE"
 
-ARTIST=$(grep 'ARTIST=' tags | cut -d= -f2)
-ALBUM=$(grep 'ALBUM=' tags | cut -d= -f2)
+ARTIST=$(grep 'ARTIST=' tags.tmp | cut -d= -f2)
+ALBUM=$(grep 'ALBUM=' tags.tmp | cut -d= -f2)
 if [ -n "$ARTIST" -a -n "$ALBUM" ] ; then
   mv -v "$OUTFILE" "$ARTIST - $ALBUM.flac"
 fi
+
+echo "Remember to do the cover:"
+echo "metaflac --import-picture-from=xyz '$ARTIST - $ALBUM.flac'"
